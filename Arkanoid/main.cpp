344e788c1056d59610be693c2c378c09faa8e7c9
sasoh <lxndrppv@gmail.com>
2013-12-04 19:48:36 +0200

    Initial Commit

---
+++ Arkanoid/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Arkanoid
+//
+//  Created by Alexander Popov on 12/4/13.
+//  Copyright (c) 2013 -. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
