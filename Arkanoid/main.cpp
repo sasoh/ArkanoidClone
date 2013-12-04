afc4dcbbb4b8d73c6670f64a1f865f0811da09fc
sasoh <lxndrppv@gmail.com>
2013-12-04 23:08:19 +0200

    Reindented main

--- Arkanoid/main.cpp
+++ Arkanoid/main.cpp
@@ -9,11 +9,11 @@
 #include <iostream>
 #include "Game.h"
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
 
     Game game;
     game.Start();
     return 0;
+    
 }
 
